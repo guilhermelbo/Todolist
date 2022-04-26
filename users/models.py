a2f3cba572c3431e9bb6f61f2d1015a6de3d4cf5
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-04-26 11:23:00 -0300

    Criado o model de User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
