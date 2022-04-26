f6bae8384f99a3f95214def8bc2cadeb84f9c4cd
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-04-26 11:00:06 -0300

    Implementação das funções valida_cadastro e valida_login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from hashlib import sha256
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from .models import User
 
 # Create your views here.
+def cadastro(request):
+    status = request.GET.get('status')
+    return render(request, 'users/cadastro.html', {'status': status})
+
+def login(request):
+    status = request.GET.get('status')
+    return render(request, 'users/login.html', {'status': status})
+
+def valida_cadastro(request):
+    usuario = request.POST.get('usuario')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    
+    if len(usuario.strip()) == 0 or len(email.strip()) == 0:
+        return redirect('/cadastro/?status=1')
+
+    if len(senha) < 8:
+        return redirect('/cadastro/?status=2')
+    
+    if User.objects.filter(name = usuario):
+        return redirect('/cadastro/?status=3')
+
+    if User.objects.filter(email = email):
+        return redirect('/cadastro/?status=4')
+
+    try:
+        senha = sha256(senha.encode()).hexdigest()
+        user = User(name = usuario, email = email, password = senha)
+        user.save()
+    except:
+        return redirect('/cadastro/?status=5')
+
+    return redirect('/')
+
+def valida_login(request):
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha = sha256(senha.encode()).hexdigest()
+
+    usuario = User.objects.filter(name = usuario).filter(password = senha)
+    
+    if usuario:
+        request.session['usuario'] = usuario[0].id
+        return redirect('/')
+    
+    return redirect('/login/?status=1')
