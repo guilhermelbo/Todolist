6e428136737296b532ef0628dd3be5125eae16ff
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-05-10 14:47:30 -0300

    Criação da tabela Task

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Task(models.Model):
+    description = models.CharField(max_length=500)
+    completed = models.BooleanField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
