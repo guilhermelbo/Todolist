5f21994720d3e9c4a24f80edfc6e6313d68e2c64
Guilherme Luiz Borges de Oliveira <gui12051@gmail.com>
2022-05-10 14:47:57 -0300

    Adicionado novas urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = "home"),
+    path('new_task/', views.new_task, name = "new_task"),
+    path('update_task/', views.update_task, name = "update_task"),
+    path('delete_task/', views.delete_task, name="delete_task"),
 ]
